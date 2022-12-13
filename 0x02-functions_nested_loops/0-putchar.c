7d8cad0ed0529c66a65d5336fe7db4b6e5ff922f
HenokB37 <117076109+HenokB37@users.noreply.github.com>
2022-12-13 07:08:08 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-  *main - Prints main as a message.
-  *this main funcion will print "_putchar" without using printf and put functions
-  *Return: Always 0 (Success)
-  */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
