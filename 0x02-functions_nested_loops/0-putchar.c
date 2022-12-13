bcda07205dc180779e30c444581eee9aa1670b05
HenokB37 <117076109+HenokB37@users.noreply.github.com>
2022-12-13 06:54:25 -0800

    c fanctions and loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  *main - Prints main as a message.
+  *this main funcion will print "_putchar" without using printf and put functions
+  *Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+  	putchar('_');
+  	putchar('p');
+  	putchar('u');
+  	putchar('t');
+  	putchar('c');
+  	putchar('h');
+  	putchar('a');
+  	putchar('r');
+  	putchar('\n');
+  	return (0);
+}
